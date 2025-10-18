7df7b911e6234d8a917443ecec696c1971e06896
Jan Stanisław Nowacki <123495481+jabluszko132@users.noreply.github.com>
2025-10-18 16:51:52 +0200

    Finish another exam

---
+++ lato 2025 poprawa/zad1/zad1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace zad1
+{
+    internal class Program
+    {
+        public static string CaesarCypher(string txt, int key)
+        {
+            string res = "";
+            int optimisedKey = key % 26;
+            foreach (char c in txt)
+            {
+                if (c == ' ')
+                {
+                    res += ' ';
+                }
+                else if (c + optimisedKey < 'a')
+                {
+                    res += (char)('z' + 1 + optimisedKey + c - 'a');
+                }else if (c + optimisedKey > 'z')
+                {
+                    res += (char)('a' - 1 + optimisedKey + c - 'z');
+                }
+                else
+                {
+                    res += (char)(c + optimisedKey);
+                }
+            }
+            return res;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Text: ");
+            string txt = Console.ReadLine()!;
+            Console.WriteLine("Key: ");
+            int key = int.Parse(Console.ReadLine()!);
+            string res = CaesarCypher(txt, key);
+            Console.WriteLine($"Cyphered with key {key}: \n{res}");
+            Console.WriteLine($"Decyphered again: \n{CaesarCypher(res,-key)}");
+        }
+    }
+}
---
+++ lato 2025 poprawa/zad2/zad2/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zad2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+
+    public partial class MainWindow : Window
+    {
+        public static string CaesarCypher(string txt, int key)
+        {
+            string res = "";
+            int optimisedKey = key % 26;
+            foreach (char c in txt)
+            {
+                if (c == ' ')
+                {
+                    res += ' ';
+                }
+                else if (c + optimisedKey < 'a')
+                {
+                    res += (char)('z' + 1 + optimisedKey + c - 'a');
+                }
+                else if (c + optimisedKey > 'z')
+                {
+                    res += (char)('a' - 1 + optimisedKey + c - 'z');
+                }
+                else
+                {
+                    res += (char)(c + optimisedKey);
+                }
+            }
+            return res;
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cypherBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string keyStr = keyIn.Text, txt = txtIn.Text;
+
+            if (keyStr == "" || txt == "")
+            {
+                MessageBox.Show("Proszę wprowadzić wszystkie wartości");
+                return;
+            }
+
+            int key = int.TryParse(keyStr, out int x) ? int.Parse(keyStr) : 0;
+
+            cypherRes.Content = CaesarCypher(txt, key);
+        }
+
+        private void saveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new();
+            if (dialog.ShowDialog() ?? false)
+            {
+                File.WriteAllText(dialog.FileName, cypherRes.Content.ToString());
+            }
+        }
+    }
+}
